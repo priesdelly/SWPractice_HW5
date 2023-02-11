d1e90c6a9de5f54da46079be5a09c001afc604d9
priesdelly <priesdelly@gmail.com>
2023-02-11 21:44:41 +0700

    change server config

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db')
 
-dotenv.config({ path: './config/config.env' });
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+
+//Connect to database
+connectDB();
+
+//Route files
+const hospitals = require('./routes/hospitals');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({
-//     success: true,
-//     data: {
-//       id: 1,
-//     },
-//   });
-// });
+//Body parser
+app.use(express.json());
 
-const hospitals = require('./routes/hospitals');
 app.use('/api/v1/hospitals', hospitals);
 
 const PORT = process.env.PORT || 5001;
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
 
-app.listen(
-  PORT,
-  console.log(
-    'Server running in ',
-    process.env.NODE_ENV,
-    ' mode on port ',
-    PORT
-  )
-);
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err,promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
